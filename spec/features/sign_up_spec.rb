2be0b495b99ec3a86d6949d2a6c21f943775719d
Shunji Kato <ShunjiKato@users.noreply.github.com>
2017-01-14 23:16:59 +0900

    Update feature test

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -8,7 +8,14 @@ feature 'Sign up' do
     fill_in 'Email', with: 'foo@example.com'
     fill_in 'Password', with: 'tarotaro'
     fill_in 'Password confirmation', with: 'tarotaro'
-    click_button 'Sign up'
+    # click_button 'Sign up'
+    expect { click_button 'Sign up' }.to change { ActionMailer::Base.deliveries.size }.by(1)
     expect(page).to have_content '確認メールを登録したメールアドレス宛に送信しました。リンクを開いてアカウントを有効にして下さい。'
+
+#    mail = ActionMailer::Base.deliveries.last
+#    body = mail.body.encoded
+#    url = body[/https[^"]+/]
+#    visit url
+#    expect(page).to have_content 'パスワードを入力して下さい'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe User, type: :model do
   it "has a valid factory" do
     expect(build(:user)).to be_valid
   end
+
+  # 重複したメールアドレスは無効な状態であること
+  it "is invalid with a duplicate email address" do
+    create(:user, email: 'hoge@example.com')
+    user = build(:user, email: 'hoge@example.com')
+    user.valid?
+    expect(user.errors[:email]).to include('は既に使用されています。')
+  end
 end
