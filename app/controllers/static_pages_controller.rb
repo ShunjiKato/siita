c33b1e0c06fdd11e6daa58f5e79792c0f27d8d62
Shunji Kato <ShunjiKato@users.noreply.github.com>
2017-01-29 00:03:29 +0900

    Add feed to home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+    if user_signed_in?
+      @drafts = current_user.drafts.all
+      if params[:id].present?
+        @draft = current_user.drafts.find(params[:id])
+      end
+    end
   end
 
   def about
