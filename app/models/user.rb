0c68a12e07a2e0f73909f15c817b436cb8e11da1
Shunji Kato <ShunjiKato@users.noreply.github.com>
2017-02-09 07:01:52 +0900

    Add user page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "follower_id",
                                   dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :active_relationships, source: :follower
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   
   resources :drafts
   resources :items
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
